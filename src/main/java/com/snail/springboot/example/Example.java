b191610532932e46507384efba9bf84c7e73607c
fzbdeqq <438267278@qq.com>
2020-09-03 23:11:14 +0800

    编译运行

--- src/main/java/com/snail/springboot/example/Example.java
+++ src/main/java/com/snail/springboot/example/Example.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class Example {
+
     @RequestMapping("/")
     public String home(){
         return "Hello world";
