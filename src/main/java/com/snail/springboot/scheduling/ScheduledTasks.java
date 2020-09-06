418c5869903915333eb07c24a9345d4a773ab804
fzbdeqq <438267278@qq.com>
2020-09-06 22:53:51 +0800

    scheduling tasks

---
+++ src/main/java/com/snail/springboot/scheduling/ScheduledTasks.java
@@ -0,0 +1,25 @@
+package com.snail.springboot.scheduling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Author: SnailBBB
+ * @Description:
+ * @Date: 2020/9/6 22:46
+ */
+@Component
+@Slf4j
+public class ScheduledTasks {
+    private static final SimpleDateFormat DATE_FORMAT=new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+//    @Scheduled(cron = "0/5 * * * * *")
+    public void reportCurrentTime(){
+        log.info("The time is now {}",DATE_FORMAT.format(new Date()));
+    }
+}
