823fdd4010ec96e8e03d4655bf8f4e30350847b2
fzbdeqq <438267278@qq.com>
2020-09-11 16:39:34 +0800

    transaction

---
+++ src/main/java/com/snail/springboot/transaction/BookingService.java
@@ -0,0 +1,31 @@
+package com.snail.springboot.transaction;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Author: SnailBBB
+ * @Description:
+ * @Date 2020/9/11 11:16
+ */
+@Service
+@Slf4j
+public class BookingService {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void book(String ...persons){
+        for (String person:persons){
+            log.info("Booking {} in a seat...",person);
+            jdbcTemplate.update("insert into bookings(first_name) values(?)",person);
+        }
+    }
+
+    public List<String> findAllBookings(){
+        return jdbcTemplate.query("select first_name from bookings",(rs,rowNum)->rs.getString("first_name"));
+    }
+}
